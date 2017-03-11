3c3e720f8ca8b776cbb2ef8476308616885a2a66
Theeeus <matheushsalves@gmail.com>
2017-03-11 10:27:37 -0500

    initial commit

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1570855826281937', // your App ID
+        'clientSecret'  : '21b66f7d78cea6ccd7567a088b246db4', // your App Secret
+        'callbackURL'   : 'http://localhost:8000/auth/facebook/callback'
+    }
+
+};
---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    'url' : 'mongodb://theus:js4dwin@ds117849.mlab.com:17849/estudos-fcc',
+    'collections' : ['nightlifersvps']
+
+};
