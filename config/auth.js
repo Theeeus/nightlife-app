653fd062035fdd84aae7f72520f652fda064909a
Theeeus <matheushsalves@gmail.com>
2017-03-11 10:56:26 -0500

    initial commit

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1570855826281937', // your App ID
         'clientSecret'  : '21b66f7d78cea6ccd7567a088b246db4', // your App Secret
-        'callbackURL'   : 'http://localhost:8000/auth/facebook/callback'
+        'callbackURL'   : 'http://remembar.herokuapp.com/auth/facebook/callback'
     }
 
 };
